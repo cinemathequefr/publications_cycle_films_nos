a1e4b182dd2a0dd217cea6a00d260eb346ad4489
Nicolas Le Thierry d'Ennequin <nlte@magic.fr>
2020-11-30 11:06:00 +0100

    Update

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,36 +3,40 @@
   import Cycle from "./Cycle.svelte";
   import _ from "lodash";
 
-  let cycles = (b =>
+  let cycles = ((b) =>
     _(b)
-      .map(c => [
+      .map((c) => [
         c[1],
-        `https://raw.githubusercontent.com/cinemathequefr/Publications_cycles/master/data/PROG75 Septembre-novembre 2020/PROG75_CYCL${c[0]} ${c[1]}/editable/PROG75_CYCL${c[0]}_FILMS_EDIT ${c[1]}.json`
-        // `https://raw.githubusercontent.com/cinemathequefr/Publications_cycles/master/data/PROG67 Mars-mai 2020/PROG67_CYCL${c[0]} ${c[1]}/editable/PROG67_CYCL${c[0]}_FILMS_EDIT ${c[1]}.json`
+        `https://raw.githubusercontent.com/cinemathequefr/Publications_cycles/master/data/PROG78 Décembre 2020-février 2021/PROG78_CYCL${c[0]} ${c[1]}/editable/PROG78_CYCL${c[0]}_FILMS_EDIT ${c[1]}.json`,
       ])
-      .sortBy(c => _.kebabCase(c[0]))
+      .sortBy((c) => _.kebabCase(c[0]))
       .value())([
-  [11, "Séances spéciales"],
-  [13, "Cinéma d'avant-garde"],
-  [14, "Cinéma bis"],
-  // [18, "Conservatoire des techniques cinématographiques"],
-  [140, "Aujourd'hui le cinéma"],
-  [152, "Parlons cinéma avec Nicolas Pariser"],
-  [463, "Andreï Kontchalovski"],
-  [409, "Don Siegel"],
-  [459, "Elizabeth Taylor"],
-  [456, "Gérard Oury"],
-  [458, "Hiroshi Shimizu"],
-  [461, "Léonide Moguy"],
-  [472, "Archi Vives"],
-  [499, "Jean-Claude Carrière"],
-  [506, "Louis de Funès"],
-  [498, "Luis Buñuel"],
-  // [508, "Semaine de la critique"],
-  [510, "Séances Jeune public"],
-  [511, "Hommage à Serge Korber"],
-  [512, "American Fringe 5"],
-  [513, "Pionnières du cinéma soviétique"]
+    [486, "Alain Resnais"],
+    [501, "Plein les yeux 4"],
+    [504, "Richard Brooks"],
+    [507, "Dino Risi"],
+    [531, "Méliès"],
+    // [11, "Séances spéciales"],
+    // [13, "Cinéma d'avant-garde"],
+    // [14, "Cinéma bis"],
+    // [18, "Conservatoire des techniques cinématographiques"],
+    // [140, "Aujourd'hui le cinéma"],
+    // [152, "Parlons cinéma avec Nicolas Pariser"],
+    // [463, "Andreï Kontchalovski"],
+    // [409, "Don Siegel"],
+    // [459, "Elizabeth Taylor"],
+    // [456, "Gérard Oury"],
+    // [458, "Hiroshi Shimizu"],
+    // [461, "Léonide Moguy"],
+    // [472, "Archi Vives"],
+    // [499, "Jean-Claude Carrière"],
+    // [506, "Louis de Funès"],
+    // [498, "Luis Buñuel"],
+    // [508, "Semaine de la critique"],
+    // [510, "Séances Jeune public"],
+    // [511, "Hommage à Serge Korber"],
+    // [512, "American Fringe 5"],
+    // [513, "Pionnières du cinéma soviétique"],
   ]);
 
   let data = [];
@@ -48,7 +52,7 @@
     data = await data.json();
 
     data = _(data)
-      .sortBy(d => _.kebabCase(d.titre))
+      .sortBy((d) => _.kebabCase(d.titre))
       .value();
   }
 </script>
@@ -67,12 +71,10 @@
         <option value={cycle[1]}>{cycle[0]}</option>
       {/each}
     </select>
-
   </div>
 </section>
 
 <section>
-
   <div class="container">
     {#if data.length > 0}
       <Cycle {data} />
